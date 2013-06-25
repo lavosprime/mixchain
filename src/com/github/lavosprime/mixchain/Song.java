6f8f9759ce602f0c51c74ce5f407344edabf4ce0
Cooper Johnson <lavosprime@gmail.com>
2013-06-24 18:14:11 -0700

    created basic Song

---
+++ src/com/github/lavosprime/mixchain/Song.java
@@ -0,0 +1,56 @@
+package com.github.lavosprime.mixchain;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Song implements Comparable<Song>, Iterable<Song> {
+
+	private int length;
+	private String title;
+	private String artist;
+	private Set<Song> transitions;
+
+	public Song(int length, String title, String artist) {
+		this.length = length;
+		this.title = title;
+		this.artist = artist;
+		this.transitions = new HashSet<Song>();
+	}
+
+	public boolean hasTransition(Song transition) {
+		return transitions.contains(transition);
+	}
+
+	public void addTransition(Song transition) {
+		transitions.add(transition);
+	}
+
+	@Override
+	public Iterator<Song> iterator() {
+		return transitions.iterator();
+	}
+
+	@Override
+	public int compareTo(Song other) {
+		if (!title.equals(other.title))
+			return title.compareTo(other.title);
+		else if (!artist.equals(other.artist))
+			return artist.compareTo(other.artist);
+		else return length - other.length;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		return this == other || other != null && other instanceof Song
+				&& title == ((Song) other).title
+				&& artist == ((Song) other).artist
+				&& length == ((Song) other).length;
+	}
+
+	@Override
+	public int hashCode() {
+		return title.hashCode();
+	}
+
+}
