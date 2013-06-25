5ba2e956bfd1485c1364a4773a6d8c5bfc1e0f4a
Cooper Johnson <lavosprime@gmail.com>
2013-06-24 23:15:49 -0700

    created MixchainMain

---
+++ src/com/github/lavosprime/mixchain/MixchainMain.java
@@ -0,0 +1,9 @@
+package com.github.lavosprime.mixchain;
+
+public class MixchainMain {
+
+    public static void main(String[] args) {
+        System.out.println("Mixchain alpha 0.0.1");
+    }
+
+}
