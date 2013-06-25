0fd7d62d91d4633084bef9cfec61b7fd1a31b7e8
Cooper Johnson <lavosprime@gmail.com>
2013-06-24 22:59:57 -0700

    finished naive FileCrawler; tweaked Song for easier FileCrawler

--- src/com/github/lavosprime/mixchain/FileCrawler.java
+++ src/com/github/lavosprime/mixchain/FileCrawler.java
@@ -6,62 +6,111 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
 public class FileCrawler {
 
-	public static final Set<String> EXTENSIONS;
+    public static final Set<String> EXTENSIONS;
 
-	static {
-		EXTENSIONS = new HashSet<String>();
-		String[] temp = new String[] {
-				"wav", "mp3", "m4a", "ogg", "flac"
-		};
-		for (String s : temp) EXTENSIONS.add(s);
-	}
+    static {
+        EXTENSIONS = new HashSet<String>();
+        String[] temp = new String[] {
+                "wav", "mp3", "m4a", "ogg", "flac"
+        };
+        for (String s : temp) EXTENSIONS.add(s);
+    }
 
-	private Scanner console;
-	private Set<Song> songs;
+    private Scanner console;
+    private Set<Song> songs;
 
-	public FileCrawler(Scanner console) {
-		this.console = console;
-		songs = new HashSet<Song>();
-	}
+    public FileCrawler(Scanner console) {
+        this.console = console;
+        songs = new HashSet<Song>();
+    }
 
-	public void crawl(File root) {
-		crawl(root, false);
-	}
+    public void crawl(File root) {
+        crawl(root, false);
+    }
 
-	public void crawl(File root, boolean useAll) {
-		crawl(root, useAll ? null : EXTENSIONS);
-	}
+    public void crawl(File root, boolean useAll) {
+        crawl(root, useAll ? null : EXTENSIONS);
+    }
 
-	public void crawl(File root, Set<String> exts) {
-		if (root.isFile()) {
-			String[] parts = makeParts(root.getName());
-			if (exts == null || exts.contains(parts[1]))
-				add(root, parts[0]);
-		}
-	}
+    public void crawl(File root, Set<String> exts) {
+        if (root.isFile()) {
+            String[] parts = makeParts(root.getName());
+            if (exts == null || exts.contains(parts[1]))
+                add(root, parts[0]);
+        } else for (File file : root.listFiles()) crawl(file, exts);
+    }
 
-	public static String[] makeParts(String fileName) {
-		String[] raw = fileName.split("[.]");
-		if (raw.length == 0)
-			return new String[] {"", ""};
-		else {
-			String[] parts = new String[2];
-			parts[0] = raw[0];
-			parts[1] = raw[raw.length - 1].toLowerCase();
-			for (int i = 1; i < raw.length - 1; i++)
-				parts[0] += "." + raw[i];
-			return parts;
-		}
-	}
+    public static String[] makeParts(String fileName) {
+        String[] raw = fileName.split("[.]");
+        if (raw.length == 0)
+            return new String[] {"", ""};
+        else {
+            String[] parts = new String[2];
+            parts[0] = raw[0];
+            parts[1] = raw[raw.length - 1].toLowerCase();
+            for (int i = 1; i < raw.length - 1; i++)
+                parts[0] += "." + raw[i];
+            return parts;
+        }
+    }
 
-	private void add(File root, String title) {
+    private void add(File file, String title) {
+        System.out.println(file.getAbsolutePath());
+        System.out.println("    Title detected: " + title);
+        System.out.print("    Use different title? (return if none) ");
+        String temp = console.nextLine().trim();
+        if (!temp.isEmpty()) title = temp;
+        do {
+            System.out.print("    Artist? ");
+            temp = console.nextLine().trim();
+        } while (temp.isEmpty());
+        String artist = temp;
+        int length = detectLength(file);
+        if (length < 0) {
+            System.out.println("    Length could not be detected.");
+            do {
+                System.out.print("    Length? (seconds) ");
+                try {
+                    length = Integer.parseInt(console.nextLine().trim());
+                } catch (NumberFormatException e) { }
+                if (length < 0)
+                    System.out.println("    Positive integer needed.");
+            } while (length < 0);
+        } else {
+            System.out.println("    Length detected: " + length + " seconds");
+            System.out.print("    Use different length? (return if none) ");
+            int temp2 = length;
+            try {
+                temp2 = Integer.parseInt(console.nextLine().trim());
+            } catch (NumberFormatException e) { }
+            if (temp2 > 0) length = temp2;
+        }
+        System.out.println();
+        Song song = new Song(title, artist, length);
+        songs.add(song);
+        System.out.println(song.toString() + " added.");
+        System.out.println();
+    }
 
-	}
+    private int detectLength(File file) {
+        try {
+            AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = stream.getFormat();
+            long frames = stream.getFrameLength();
+            return (int) ((frames + 0.0) / format.getFrameRate());
+        } catch (Exception e) {
+            return -1;
+        }
+    }
 
-	public Set<Song> results() {
-		return Collections.unmodifiableSet(songs);
-	}
+    public Set<Song> results() {
+        return Collections.unmodifiableSet(songs);
+    }
 
 }
--- src/com/github/lavosprime/mixchain/Song.java
+++ src/com/github/lavosprime/mixchain/Song.java
@@ -6,51 +6,56 @@ import java.util.Set;
 
 public class Song implements Comparable<Song>, Iterable<Song> {
 
-	private int length;
-	private String title;
-	private String artist;
-	private Set<Song> transitions;
+    private int length;
+    private String title;
+    private String artist;
+    private Set<Song> transitions;
 
-	public Song(int length, String title, String artist) {
-		this.length = length;
-		this.title = title;
-		this.artist = artist;
-		this.transitions = new HashSet<Song>();
-	}
+    public Song(String title, String artist, int length) {
+        this.length = length;
+        this.title = title;
+        this.artist = artist;
+        this.transitions = new HashSet<Song>();
+    }
 
-	public boolean hasTransition(Song transition) {
-		return transitions.contains(transition);
-	}
+    public boolean hasTransition(Song transition) {
+        return transitions.contains(transition);
+    }
 
-	public void addTransition(Song transition) {
-		transitions.add(transition);
-	}
+    public void addTransition(Song transition) {
+        transitions.add(transition);
+    }
 
-	@Override
-	public Iterator<Song> iterator() {
-		return transitions.iterator();
-	}
+    @Override
+    public Iterator<Song> iterator() {
+        return transitions.iterator();
+    }
 
-	@Override
-	public int compareTo(Song other) {
-		if (!title.equals(other.title))
-			return title.compareTo(other.title);
-		else if (!artist.equals(other.artist))
-			return artist.compareTo(other.artist);
-		else return length - other.length;
-	}
+    @Override
+    public int compareTo(Song other) {
+        if (!title.equals(other.title))
+            return title.compareTo(other.title);
+        else if (!artist.equals(other.artist))
+            return artist.compareTo(other.artist);
+        else return length - other.length;
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		return this == other || other != null && other instanceof Song
-				&& title == ((Song) other).title
-				&& artist == ((Song) other).artist
-				&& length == ((Song) other).length;
-	}
+    @Override
+    public boolean equals(Object other) {
+        return this == other || other != null && other instanceof Song
+                && title == ((Song) other).title
+                && artist == ((Song) other).artist
+                && length == ((Song) other).length;
+    }
 
-	@Override
-	public int hashCode() {
-		return title.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + title + "\" by " + artist + " (" + length + " s)";
+    }
 
 }
